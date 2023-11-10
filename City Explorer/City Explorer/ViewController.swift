b1e508628446610d46a574e25a59c13e7c172fe1
Brian Kwong <bkwong01@calpoly.edu>
2023-11-10 02:46:47 -0800

    Added UI
    Updates

---
+++ City Explorer/City Explorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  City Explorer
+//
+//  Created by brian on 11/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
